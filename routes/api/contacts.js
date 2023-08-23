6d49cd2e14af9d7a37579fd8d5d610ad938ac954
Bohdan <yashbon@ukr.net>
2023-08-23 23:25:12 +0300

    hw02: add controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,33 +1,12 @@
 const express = require("express");
+
 const router = express.Router();
-
-const Joi = require("joi");
-
-const {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-    updateContact,
-} = require("../../models/contacts");
-
-const { HttpError } = require("../../utils");
 const ctrl = require("../../controllers/contacts");
 
-const addContactSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
-});
-
 router.get("/", ctrl.getAllContacts);
-
 router.get("/:contactId", ctrl.getContactById);
-
 router.post("/", ctrl.addContact);
-
 router.delete("/:contactId", ctrl.removeContact);
-
 router.put("/:contactId", ctrl.updateContact);
 
 module.exports = router;
