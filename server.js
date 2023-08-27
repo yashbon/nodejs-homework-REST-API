65df2017b05e7f1a9eb53b11fec25babb3e69605
Bohdan <yashbon@ukr.net>
2023-08-27 22:57:29 +0300

    hw03-nomgodb: Add .env (dotenv)

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-    "mongodb+srv://yashbon:phonebook@cluster0.ohfd8dg.mongodb.net/phonebook?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 mongoose.set("strictQuery", true);
 mongoose
     .connect(DB_HOST)
