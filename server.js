a7cd9d2e199230524ac547f6acbf167b55bbe3b4
Bohdan <yashbon@ukr.net>
2023-08-27 18:21:38 +0300

    changed .gitignor

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose
         app.listen(3000, () => {
             console.log("Server running. Use our API on port: 3000");
         });
-        console.log("Databse connect success");
+        console.log("Database connect success");
     })
     .catch((error) => {
         console.log(error.message);
