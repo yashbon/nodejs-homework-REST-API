8199ecc7ca3b3c245c2e4c58aa1fd3e859bf29e8
Bohdan <yashbon@ukr.net>
2023-08-27 17:23:53 +0300

    hw03-mongodb: Created the connect to DB

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+    "mongodb+srv://yashbon:phonebook@cluster0.ohfd8dg.mongodb.net/phonebook?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+mongoose
+    .connect(DB_HOST)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Server running. Use our API on port: 3000");
+        });
+        console.log("Databse connect success");
+    })
+    .catch((error) => {
+        console.log(error.message);
+        process.exit(1);
+    });
