63c51ed2eef97960f2577eb59a53c52381225404
Bohdan <yashbon@ukr.net>
2023-09-18 01:32:33 +0300

    hw06-email: completed Step-4

--- controllers/users.js
+++ controllers/users.js
@@ -65,7 +65,27 @@ const verifyEmail = async (req, res) => {
     });
 };
 
-const resendVerifyEmail = async (req, res) => {};
+const resendVerifyEmail = async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw HttpError(400, "missing required field email");
+    }
+    if (user.verify) {
+        throw HttpError(400, "Verification has already been passed");
+    }
+
+    const { BASE_URL } = process.env;
+    const verifyEmail = {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click verify email</a>`,
+    };
+    await sendEmail(verifyEmail(email, user.verificationToken));
+
+    res.json({ massage: "Verification email sent" });
+};
 
 const login = async (req, res) => {
     const { email, password } = req.body;
